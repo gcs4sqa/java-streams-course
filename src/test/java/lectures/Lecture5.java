f5b3f1c388273f1ffc94e9298a6b8fdf2b6356f0
cliff.douglas@digital.homeoffice.gov.uk <!apple01apple01>
2022-09-06 13:59:42 +0100

    complete lecture4.  started on lecture5

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -15,16 +15,36 @@ import org.junit.Test;
 
 public class Lecture5 {
 
+ 
+
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
+    List<Car> carsFiltered = cars.stream()
+    .filter(carPredicate)
+    .collect(Collectors.toList());
+
+    cars.forEach(System.out::println);
+    System.out.println(carsFiltered.size());
 
   }
+
+final Predicate<Car> carPredicate = car -> car.getPrice() < 10000;
 
   @Test
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
+
+    List<PersonDTO> dtos = people.stream()
+    .map(person -> {
+      PersonDTO dto = new PersonDTO(person.getId(), person.getFirstName(), person.getAge());
+      return dto;
+    })
+    .collect(Collectors.toList());
+    dtos.forEach(System.out::println);
+    assertThat(dtos).hasSize(1000);
+    System.out.println(dtos.size());
 
   }
 
